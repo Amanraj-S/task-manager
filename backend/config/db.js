1b5ad75cc288219d34696573d8c944d560cdef46
Amanraj S <amanrajsathishkumar05@gmail.com>
2025-08-24 15:56:13 +0530

    first commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  try{
-    await mongoose.connect(process.env.mongo_URI, {});
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {}); 
     console.log("MongoDB Connected");
   } catch (err) {
     console.error("Error connecting to MongoDB", err);
