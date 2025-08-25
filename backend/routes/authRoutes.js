b8756559addab8a741732c39cb78995fe6b61376
Amanraj S <amanrajsathishkumar05@gmail.com>
2025-08-25 10:07:31 +0530

    first commit

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -15,8 +15,12 @@ router.post("/upload-image", upload.single("image"), (req, res) => {
     if (!req.file) {
         return res.status(400).json({ message: "No file uploaded" });
     }
-    const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`;
+
+   
+    const imageUrl = `https://${req.get("host")}/uploads/${req.file.filename}`;
+    
     res.status(200).json({ imageUrl });
 });
+
 
 module.exports = router;
